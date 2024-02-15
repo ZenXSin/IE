356dd62cdf0d782f9ebd7ff4a3ac3dd92f602508
ZenXSin <488254306@qq.com>
2024-02-15 11:31:11 +0800

    Update 电浆流.kt

--- src/world/电浆流.kt
+++ src/world/电浆流.kt
@@ -1,13 +1,18 @@
 package world
 
 import mindustry.Vars
+import mindustry.ctype.ContentType
 import mindustry.world.blocks.liquid.*
 
 fun getLiquid() {
     Vars.world.tiles.eachTile {
         val block = it.block()
-        if ((block is Conduit || block is ArmoredConduit || block is LiquidRouter || block is LiquidJunction || block is LiquidBridge) && !block.name.contains("磁约束")) {
-            it.build.kill()
+        if (((block is Conduit || block is ArmoredConduit || block is LiquidRouter || block is LiquidJunction || block is LiquidBridge) && it.build.liquids.get(Vars.content.getByName(ContentType.liquid,"工业拓展-电浆流")) > 0.06f) && !block.name.contains("磁约束")) {
+                try {
+                    if (it.build != null){
+                        it.build.kill()
+                    }
+                } catch (a:Exception) {}
         }
     }
 }
